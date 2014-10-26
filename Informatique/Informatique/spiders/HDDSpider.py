3af168941f37127c0588593430cd9cb8e08a523e
Hoye <hoyelam96@gmail.com>
2014-10-26 17:29:20 +0100

    Informatique 100%

---
+++ Informatique/Informatique/spiders/HDDSpider.py
@@ -0,0 +1,34 @@
+import scrapy
+
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+
+
+from Informatique.items import  InformatiqueItem
+
+class CasesSpider(CrawlSpider):
+    name = "INF_HDD"
+    allowed_domains = ["informatique.nl"]
+    start_urls = ["http://www.informatique.nl/?m=usl&g=026&view=6&&sort=pop&pl=500",
+    
+ ]
+    
+   #rules = (Rule (SgmlLinkExtractor(restrict_xpaths=('//a[contains(., "Volgende")]',))
+   # , callback="parse_start_url", follow= True),
+   #)
+    
+    def parse_start_url(self,response):
+        hxs = HtmlXPathSelector(response)
+        titles = hxs.select('//ul[@id="detailview"]/li')
+        items = []
+        for titles in titles:
+           item = InformatiqueItem()
+           item['webshop'] = 'Informatique'
+           item['name'] = titles.select('div[@id="title"]/a/text()').extract()
+           item['url'] = titles.select('div[@id="title"]/a/@href').extract()
+           item['desc'] = titles.select('div[@id="description"]/ul/li/text()').extract()
+           item['price'] = titles.select('div[@id="price"]/text()').extract()
+           item['image_urls'] = titles.select('div[@id="image"]/a/img/@src').extract()
+           items.append(item)
+        return (items)
