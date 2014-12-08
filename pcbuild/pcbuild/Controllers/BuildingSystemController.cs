f9b9543fde2a2180310a0a96bb70b0bd100d9404
Hoye <hoyelam96@gmail.com>
2014-12-08 21:07:38 +0100

    pcbuild website start
    nog erg 1.0

---
+++ pcbuild/pcbuild/Controllers/BuildingSystemController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace pcbuild.Controllers
+{
+    public class BuildingSystemController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ pcbuild/pcbuild/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pcbuild.Startup))]
+namespace pcbuild
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
