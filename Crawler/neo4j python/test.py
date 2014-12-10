69e89b381d90dd44327d5a945503bccd96d16b04
Falio <f.fremouw@gmail.com>
2014-12-10 16:38:12 +0100

    titelfilter ook aan hardware toegevoegd

--- Crawler/neo4j python/test.py
+++
@@ -1,7 +0,0 @@
-from py2neo import neo4j
-from py2neo import node, rel
-
-graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-a, b, ab = query.execute(name_a="Alice", name_b="Bob").data[0]
-query = neo4j.CypherQuery(graph_db, "CREATE (a {name:{name_a}})-[ab:KNOWS]->(b {name:{name_b}})"
-                              "RETURN a, b, ab")
