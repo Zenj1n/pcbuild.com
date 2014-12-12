29145437304a2172c60d4fb0152ef7ebf4d194b6
Hoye <hoyelam96@gmail.com>
2014-12-12 13:22:19 +0100

    Commit Pcbuild
    Website

--- Crawler/Informatique/Informatique/spiders/CasesSpider.py
+++ Crawler/Informatique/Informatique/spiders/CasesSpider.py
@@ -7,6 +7,7 @@ from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 from py2neo import rel, node
 from py2neo import neo4j
+from py2neo import Graph
 
 
 from Informatique.items import  InformatiqueItem
@@ -21,7 +22,7 @@ class CasesSpider(CrawlSpider):
     )
     
     def parse_start_url(self,response):
-        #graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+        graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
         hxs = Selector(response)
         type = hxs.xpath('div[@id="wrapper_content"]/div[@id="hdr"]/h1/text()')
         titles = hxs.xpath('//ul[@id="detailview"]/li')
@@ -36,7 +37,7 @@ class CasesSpider(CrawlSpider):
            print webshop, name, url, desc, price, type
            print "== Adding Node to database =="
         
-           #query = neo4j.CypherQuery(graph_db, "CREATE (inf_case {webshop:{webshop}, name:{name}, url:{url}, desc:{desc}, price:{price}})"
-           #                   "RETURN inf_case")
+           query = neo4j.CypherQuery(graph_db, "CREATE (inf_case {webshop:{webshop}, name:{name}, url:{url}, desc:{desc}, price:{price}})"
+                              "RETURN inf_case")
                               
-           #inf_case = query.execute(webshop=webshop, name=name, url=url, desc=desc, price=price)
+           inf_case = query.execute(webshop=webshop, name=name, url=url, desc=desc, price=price)
---
+++ pcbuild/pcbuild/Controllers/ComponentenController.cs
@@ -0,0 +1,23 @@
+﻿using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pcbuild.Controllers
+{
+    public class ComponentenController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+                client.Connect();
+            
+            
+            return View();
+        }
+    }
+
+}
