d8de8e38a998aa1cd4d5f0c9ca16c1f195202356
Hoye <hoyelam96@gmail.com>
2014-12-17 17:50:32 +0100

    nog steeds met dataweergave op website bezig
    Website ziet de nodes, maar haalt geen data eruit

--- pcbuild/pcbuild/Controllers/ComponentenController.cs
+++ pcbuild/pcbuild/Controllers/ComponentenController.cs
@@ -19,7 +19,7 @@ namespace pcbuild.Controllers
 {
     public class ComponentenController : Controller
     {
-
+      
         public ActionResult Index()
         {
             var componenten = new List<All_Components>();
@@ -28,33 +28,39 @@ namespace pcbuild.Controllers
 
             WebClient c = new WebClient();
             var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
-                client.Connect();
+            client.Connect();
 
+            var componenten_query = client
+                .Cypher
+                .Match("n")
+                .Return<All_Components>("n");
+                ;
 
-                try
-                {
-                    var componenten_query = client
-                        .Cypher
-                        .Match("(n)")
-                        .Return(n => n.As<All_Components>())
-                        ;
+            var test = componenten_query.Results.ToList();
 
-                    var test = componenten_query.Results;
+                //var test = componenten_query.Results;
                    // Debug.WriteLine(test.ToList);
 
-                    foreach (var item in test.ToList())
-                    {
-                        Debug.WriteLine("1", item.All_Components_Name);
-                    }
+            foreach (var item in test)
+            {
+                Debug.WriteLine("1", item.All_Components_Name);
+                Debug.WriteLine("2", item.All_Components_Desc);
+                Debug.WriteLine("3", item.All_Components_URL);
+                Debug.WriteLine("4", item.All_Components_Price);
+                Debug.WriteLine("5", item.All_Components_Webshop);
+                Debug.WriteLine("6", item.All_Components_Component);
+            }
 
-                    //ToString(x.All_Components_Name);                    
-                    //Debug.WriteLine(x.All_Components_Name);
-                    Debug.WriteLine("test_string");
-                }
-                catch (Exception e)
-                {
-                    Debug.WriteLine(e);
-                }
+                //foreach (var item in test)
+                //{
+                //Debug.WriteLine("1",item.All_Components_Name.ToString());
+                //Debug.WriteLine("2", item.All_Components_Desc.ToString());
+                //Debug.WriteLine("3", item.All_Components_URL.ToString());
+                //Debug.WriteLine("4", item.All_Components_Price.ToString());
+                //Debug.WriteLine("5", item.All_Components_Webshop.ToString());
+                //Debug.WriteLine("6", item.All_Components_Component.ToString());
+                //}
+                Debug.WriteLine("test_string");
 
                 for (int i = 0; i < 100; i++){
                     componenten2.Add("yo");
@@ -69,9 +75,7 @@ namespace pcbuild.Controllers
 
                 //    componenten.Add(component);
                 //}
-                     
-            
-            return View(componenten);
+                return View();
         }
     }
 }
