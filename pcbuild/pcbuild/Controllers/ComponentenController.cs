0ee3862c566d45a3051ad5a5dc11d26ea0aae067
Hoye <hoyelam96@gmail.com>
2014-12-17 15:03:21 +0100

    Connectie met database klopt
    Alleen nog dataweergeven

--- pcbuild/pcbuild/Controllers/ComponentenController.cs
+++ pcbuild/pcbuild/Controllers/ComponentenController.cs
@@ -7,8 +7,15 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using System.Collections;
+using Neo4jClient.Cypher;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
-namespace pcbuild.Controllers
+
+namespace pcbuild.Controllers   
 {
     public class ComponentenController : Controller
     {
@@ -17,46 +24,51 @@ namespace pcbuild.Controllers
         {
             var componenten = new List<All_Components>();
             ArrayList componenten2 = new ArrayList();
+            //All_Components x = new All_Components();
+
+            WebClient c = new WebClient();
             var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
                 client.Connect();
 
-                //var test = client.Cypher
-                //    .Match("(Movie:Movie)")
-                //    .Return(Movie => Movie.As<Movie>());
-                //var test2 = test.Results;
-                //Console.WriteLine(test2);
-            
-                //var Test = client
-                //    .Cypher
-                //    .Match("(Movie:Movie")
-                //    .Return<Node<Movie>>("Movie")
-                //    .Results;
-                //Console.WriteLine(Movie.title);
-                //Console.ReadLine();
+
+                try
+                {
+                    var componenten_query = client
+                        .Cypher
+                        .Match("(n)")
+                        .Return(n => n.As<All_Components>())
+                        ;
+
+                    var test = componenten_query.Results;
+                   // Debug.WriteLine(test.ToList);
+
+                    foreach (var item in test.ToList())
+                    {
+                        Debug.WriteLine("1", item.All_Components_Name);
+                    }
+
+                    //ToString(x.All_Components_Name);                    
+                    //Debug.WriteLine(x.All_Components_Name);
+                    Debug.WriteLine("test_string");
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e);
+                }
+
                 for (int i = 0; i < 100; i++){
                     componenten2.Add("yo");
                 }
 
-                // Code als connectie met database werkt moet nog met Rinesh bespreken hoe.
-                //foreach (var all_component in componenten)
+                //foreach (var all_component in componenten2)
                 //{
                 //    var component = new All_Components();
 
-                //    component.All_Components_Name = ;
-                //    component.All_Components_Price = ;
+                //    component.All_Components_Name = "WDD GREEN 2TB";
+                //    component.All_Components_Price = 90;
 
                 //    componenten.Add(component);
                 //}
-
-                foreach (var all_component in componenten2)
-                {
-                    var component = new All_Components();
-
-                    component.All_Components_Name = "WDD GREEN 2TB";
-                    component.All_Components_Price = 90;
-
-                    componenten.Add(component);
-                }
                      
             
             return View(componenten);
--- pcbuild/pcbuild/Models/All_Components.cs
+++ pcbuild/pcbuild/Models/All_Components.cs
@@ -7,11 +7,11 @@ namespace pcbuild.Models
 {
     public class All_Components
     {
-        public int All_Components_ID { get; set; }
+        //public int All_Components_ID { get; set; }
         public string All_Components_Name { get; set; }
         public string All_Components_Desc { get; set; }
-        public string All_Components_URL { get; set; }
+        //public string All_Components_URL { get; set; }
         public decimal All_Components_Price { get; set; }
-        public string All_Components_Webshop { get; set; }
+        //public string All_Components_Webshop { get; set; }
     }
 }
