66c3476219aa45446d9b2917117d47fac88bfd7e
Hoye <hoyelam96@gmail.com>
2015-01-07 15:52:07 +0100

    behuizing pagina data weergave

---
+++ pcbuild/pcbuild/Controllers/BehuizingController.cs
@@ -0,0 +1,55 @@
+﻿using Neo4jClient;
+using pcbuild.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Collections;
+using Neo4jClient.Cypher;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace pcbuild.Controllers
+{
+    public class BehuizingController : Controller
+    {
+        // GET: Behuizing
+        public ActionResult Index()
+        {
+            //Connectie met database
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+            client.Connect();
+
+            //Query om alle componenten opte halen
+            var componenten_query = client
+                .Cypher
+                .Match("(n:behuizing)")
+                .Return(n => n.As<Behuizing_Model>())
+                .Results;
+
+            //Test om te kijken of er data in de model zit
+            foreach (var item in componenten_query)
+            {
+                if (item.naam == null)
+                {
+                    Debug.WriteLine("Geen data");
+                }
+                else
+                {
+
+                    Debug.WriteLine("1", item.naam);
+                    Debug.WriteLine("2", item.interfaces);
+                    Debug.WriteLine("3", item.vormfactor);
+                    Debug.WriteLine("4", item.vormvoeding);
+                }
+
+            }
+            return View(componenten_query);
+        }
+    }
+}
---
+++ pcbuild/pcbuild/Models/Behuizing_Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pcbuild.Models
+{
+    public class Behuizing_Model
+    {
+        public string naam { get; set; }
+        public string interfaces { get; set; }
+        public string vormfactor { get; set; }
+        public string vormvoeding { get; set; }
+        public string webshop { get; set; }
+    }
+}
