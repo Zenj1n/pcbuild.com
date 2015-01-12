755a5c6a723eee1aed5f22fa64d86e8b4f71405c
Hoye <hoyelam96@gmail.com>
2015-01-12 12:30:19 +0100

    random commit om te syncen

--- pcbuild/pcbuild/Controllers/HomeController.cs
+++ pcbuild/pcbuild/Controllers/HomeController.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using Neo4jClient;
+using pcbuild.Models;
+using pcbuild.Models.ProcessorModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Collections;
+using Neo4jClient.Cypher;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace pcbuild.Controllers
 {
@@ -25,6 +36,28 @@ namespace pcbuild.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ProcessorStart()
+        {
+
+            //Connectie met database
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+            client.Connect();
+
+            // Query om alle behuizingen op te halen
+            var componenten_query = client
+              .Cypher
+              .Match("(n:processor)-[r:verkrijgbaar]-(p:Webshop)")
+              .Return((n, r, p) => new ViewModelProcessor
+              {
+                  Proccesor_m = n.As<Processor_Model>(),
+                  Verkrijgbaar_m = r.As<Verkrijgbaar_Model>(),
+                  Webshop_m = p.As<Webshop_Model>(),
+              })
+              .Results;
+
+            return View(componenten_query);
         }
     }
 }
