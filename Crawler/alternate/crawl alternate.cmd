a7f6bf16f11188e786cbcd50cbb47dfea9950e15
Falio <f.fremouw@gmail.com>
2015-01-12 11:31:57 +0100

    cmd

--- Crawler/alternate/crawl alternate.cmd
+++ Crawler/alternate/crawl alternate.cmd
@@ -1,14 +1,12 @@
 scrapy crawl alt_cpu
 scrapy crawl alt_case
-scrapp crawl alt_tvcard
 scrapy crawl alt_drive
 scrapy crawl alt_cpu
 scrapy crawl alt_gfx
 scrapy crawl alt_hd
-scrapy crawl alt_koel_case
 scrapy crawl alt_mb
 scrapy crawl alt_nwcard
 scrapy crawl alt_psu
-scrapy crawl alt_ram_ddr
+scrapy crawl alt_ram
 scrapy crawl alt_soundcard
 scrapy crawl alt_ssd
