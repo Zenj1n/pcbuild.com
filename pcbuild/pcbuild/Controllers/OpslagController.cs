4393b9d203c946d77bdd58ec02d2073c51cfdf25
Hoye <hoyelam96@gmail.com>
2015-01-13 11:46:27 +0100

    alle stappen verbonden

---
+++ pcbuild/pcbuild/Controllers/OpslagController.cs
@@ -0,0 +1,45 @@
+﻿using Neo4jClient;
+using pcbuild.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Collections;
+using Neo4jClient.Cypher;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using pcbuild.Models.OpslagModels;
+
+namespace pcbuild.Controllers
+{
+    public class OpslagController : Controller
+    {
+        // GET: Opslag
+        public ActionResult Index()
+        {
+
+            //Connectie met database
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+            client.Connect();
+
+            // Query om alle behuizingen op te halen
+            var componenten_query = client
+              .Cypher
+              .Match("(n:opslag)-[r:verkrijgbaar]-(p:Webshop)")
+              .Return((n, r, p, z) => new ViewModelOpslag
+              {
+                  Opslag_m = n.As<Opslag_Model>(),                  
+                  Verkrijgbaar_m = r.As<Verkrijgbaar_Model>(),
+                  Webshop_m = p.As<Webshop_Model>(),
+              })
+              .Results;
+
+            return View(componenten_query);
+        }
+    }
+}
---
+++ pcbuild/pcbuild/Models/OpslagModels/ViewModelOpslag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pcbuild.Models.OpslagModels
+{
+    public class ViewModelOpslag
+    {
+        public Opslag_Model Opslag_m{ get; set; }
+        public Verkrijgbaar_Model Verkrijgbaar_m { get; set; }
+        public Webshop_Model Webshop_m { get; set; }
+    }
+}
