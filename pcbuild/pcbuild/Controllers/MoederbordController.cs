e494bfa77d63d4eec350056e3e814f29904886c9
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2015-01-14 18:13:45 +0100

    cookies and list added
    added cookies and list.

--- pcbuild/pcbuild/Controllers/MoederbordController.cs
+++ pcbuild/pcbuild/Controllers/MoederbordController.cs
@@ -22,12 +22,58 @@ namespace pcbuild.Controllers
         // GET: Moederbord
         public ActionResult Index(string processor, string socket, string prijs)
         {
+            //Maak cookie arrays
+            HttpCookie processor_cookie = new HttpCookie("processor_cookie");
+            HttpCookie moederbord_cookie = new HttpCookie("moederbord_cookie");
+            HttpCookie videokaart_cookie = new HttpCookie("videokaart_cookie");
+            HttpCookie werkgeheugen_cookie = new HttpCookie("werkgeheugen_cookie");
+            HttpCookie behuizing_cookie = new HttpCookie("behuizing_cookie");
+            HttpCookie opslag_cookie = new HttpCookie("opslag_cookie");
+            HttpCookie voeding_cookie = new HttpCookie("voeding_cookie");
 
-            Debug.WriteLine(processor);
-            Debug.WriteLine(socket);
-            Debug.WriteLine(prijs);
+            HttpCookie processorprijs_cookie = new HttpCookie("processorprijs_cookie");
+            HttpCookie moederbordprijs_cookie = new HttpCookie("moederbordprijs_cookie");
+            HttpCookie videokaartprijs_cookie = new HttpCookie("videokaartprijs_cookie");
+            HttpCookie werkgeheugenprijs_cookie = new HttpCookie("werkgeheugenprijs_cookie");
+            HttpCookie behuizingprijs_cookie = new HttpCookie("behuizingprijs_cookie");
+            HttpCookie opslagprijs_cookie = new HttpCookie("opslagprijs_cookie");
+            HttpCookie voedingprijs_cookie = new HttpCookie("voedingprijs_cookie");
 
+            //voeg data toe aan cookies
+            processor_cookie.Value = processor;
+            moederbord_cookie.Value = "Geen moederbord toegevoegd.";
+            videokaart_cookie.Value = "Geen videokaart toegevoegd.";
+            werkgeheugen_cookie.Value = "Geen werkgeheugen toegevoegd.";
+            behuizing_cookie.Value = "Geen behuizing toegevoegd.";
+            opslag_cookie.Value = "Geen opslag toegevoegd.";
+            voeding_cookie.Value = "Geen voeding toegevoegd.";
 
+            processorprijs_cookie.Value = prijs;
+            moederbordprijs_cookie.Value = "€ 0,-";
+            videokaartprijs_cookie.Value = "€ 0,-"; ;
+            werkgeheugenprijs_cookie.Value = "€ 0,-";
+            behuizingprijs_cookie.Value = "€ 0,-";
+            opslagprijs_cookie.Value = "€ 0,-";
+            voedingprijs_cookie.Value = "€ 0,-";
+
+            //save the cookies!!!
+            Response.Cookies.Add(processor_cookie);
+            Response.Cookies.Add(moederbord_cookie);
+            Response.Cookies.Add(videokaart_cookie);
+            Response.Cookies.Add(werkgeheugen_cookie);
+            Response.Cookies.Add(behuizing_cookie);
+            Response.Cookies.Add(opslag_cookie);
+            Response.Cookies.Add(voeding_cookie);
+
+            Response.Cookies.Add(processorprijs_cookie);
+            Response.Cookies.Add(moederbordprijs_cookie);
+            Response.Cookies.Add(videokaartprijs_cookie);
+            Response.Cookies.Add(werkgeheugenprijs_cookie);
+            Response.Cookies.Add(behuizingprijs_cookie);
+            Response.Cookies.Add(opslagprijs_cookie);
+            Response.Cookies.Add(voedingprijs_cookie);
+
+            Debug.WriteLine(processor_cookie.Value);
 
             //Connectie met database
             var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
--- pcbuild/pcbuild/Controllers/ProcessorController.cs
+++ pcbuild/pcbuild/Controllers/ProcessorController.cs
@@ -24,6 +24,57 @@ namespace pcbuild.Controllers
         // GET: Processor
         public ActionResult Index()
         {
+            //Maak cookie arrays
+            HttpCookie processor_cookie = new HttpCookie("processor_cookie");
+            HttpCookie moederbord_cookie = new HttpCookie("moederbord_cookie");
+            HttpCookie videokaart_cookie = new HttpCookie("videokaart_cookie");
+            HttpCookie werkgeheugen_cookie = new HttpCookie("werkgeheugen_cookie");
+            HttpCookie behuizing_cookie = new HttpCookie("behuizing_cookie");
+            HttpCookie opslag_cookie = new HttpCookie("opslag_cookie");
+            HttpCookie voeding_cookie = new HttpCookie("voeding_cookie");
+
+            HttpCookie processorprijs_cookie = new HttpCookie("processorprijs_cookie");
+            HttpCookie moederbordprijs_cookie = new HttpCookie("moederbordprijs_cookie");
+            HttpCookie videokaartprijs_cookie = new HttpCookie("videokaartprijs_cookie");
+            HttpCookie werkgeheugenprijs_cookie = new HttpCookie("werkgeheugenprijs_cookie");
+            HttpCookie behuizingprijs_cookie = new HttpCookie("behuizingprijs_cookie");
+            HttpCookie opslagprijs_cookie = new HttpCookie("opslagprijs_cookie");
+            HttpCookie voedingprijs_cookie = new HttpCookie("voedingprijs_cookie");
+
+            //voeg data toe aan cookies
+            processor_cookie.Value = "Geen processor toegevoegd.";
+            moederbord_cookie.Value = "Geen moederbord toegevoegd.";
+            videokaart_cookie.Value = "Geen videokaart toegevoegd.";
+            werkgeheugen_cookie.Value = "Geen werkgeheugen toegevoegd.";
+            behuizing_cookie.Value = "Geen behuizing toegevoegd.";
+            opslag_cookie.Value = "Geen opslag toegevoegd.";
+            voeding_cookie.Value = "Geen voeding toegevoegd.";
+
+            processorprijs_cookie.Value = "€ 0,-";
+            moederbordprijs_cookie.Value = "€ 0,-";
+            videokaartprijs_cookie.Value = "€ 0,-"; ;
+            werkgeheugenprijs_cookie.Value = "€ 0,-";
+            behuizingprijs_cookie.Value = "€ 0,-";
+            opslagprijs_cookie.Value = "€ 0,-";
+            voedingprijs_cookie.Value = "€ 0,-";
+
+            //save the cookies!!!
+            Response.Cookies.Add(processor_cookie);
+            Response.Cookies.Add(moederbord_cookie);
+            Response.Cookies.Add(videokaart_cookie);
+            Response.Cookies.Add(werkgeheugen_cookie);
+            Response.Cookies.Add(behuizing_cookie);
+            Response.Cookies.Add(opslag_cookie);
+            Response.Cookies.Add(voeding_cookie);
+
+            Response.Cookies.Add(processorprijs_cookie);
+            Response.Cookies.Add(moederbordprijs_cookie);
+            Response.Cookies.Add(videokaartprijs_cookie);
+            Response.Cookies.Add(werkgeheugenprijs_cookie);
+            Response.Cookies.Add(behuizingprijs_cookie);
+            Response.Cookies.Add(opslagprijs_cookie);
+            Response.Cookies.Add(voedingprijs_cookie);
+
             //Connectie met database
             var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
             client.Connect();
@@ -39,8 +90,6 @@ namespace pcbuild.Controllers
                   Webshop_m = p.As<Webshop_Model>(),
               })
               .Results;
-
-            Debug.WriteLine(componenten_query);
 
             return View(componenten_query);
         }
