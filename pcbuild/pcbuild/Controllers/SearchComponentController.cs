3074db348f85f917ff5005c2447605a12d2fd9cd
Hoye <hoyelam96@gmail.com>
2015-01-19 13:33:27 +0100

    Search Controller begin

---
+++ pcbuild/pcbuild/Controllers/SearchComponentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pcbuild.Controllers
+{
+    public class SearchComponentController : Controller
+    {
+        // GET: SearchComponent
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
