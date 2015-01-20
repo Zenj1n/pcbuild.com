d686afb2fe4d31adc74d07b8e02d5562e082c5d7
Falio <f.fremouw@gmail.com>
2015-01-20 16:10:58 +0100

    eerste succesvolle untit test!!! :smiley:

---
+++ pcbuild/UnitTestProjectPcbuild/CookieTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectPcbuild
+{
+    [TestClass]
+    public class CookieTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
