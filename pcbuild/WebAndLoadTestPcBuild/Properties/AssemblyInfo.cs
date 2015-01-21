32fc479f5221225aa292f57da505f7331f75f0fb
Falio <f.fremouw@gmail.com>
2015-01-21 15:48:22 +0100

    web and load test

--- pcbuild/WebAndLoadTestPcBuild/Properties/AssemblyInfo.cs
+++ pcbuild/WebAndLoadTestPcBuild/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTestProjectPcbuild")]
+[assembly: AssemblyTitle("WebAndLoadTestPcBuild")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProjectPcbuild")]
+[assembly: AssemblyProduct("WebAndLoadTestPcBuild")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("88dae2df-e28f-490b-a3c4-2adfb178a7fa")]
+[assembly: Guid("11622e37-7459-4e57-b4d7-b19f22ce0897")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
