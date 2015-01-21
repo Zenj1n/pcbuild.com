59091287d8016e2a4432ce0174c2005222892503
Jtlie <jtlie@live.nl>
2015-01-21 15:23:59 +0100

    dashboard

--- Dashboard - Jason/Dashboard/Dashboard/Form1.Designer.cs
+++ Dashboard - Jason/Dashboard/Dashboard/Form1.Designer.cs
@@ -37,11 +37,35 @@
             System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series21 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Aantal_onderdelen_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Avg_Prijzen_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Totale_Prijzen_Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.Aantal_onderdelen_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Avg_Prijzen_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Totale_Prijzen_Chart)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -52,6 +76,7 @@
             this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(155, 39);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Dashboard";
@@ -64,7 +89,7 @@
             this.Aantal_onderdelen_chart.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.Aantal_onderdelen_chart.Legends.Add(legend1);
-            this.Aantal_onderdelen_chart.Location = new System.Drawing.Point(84, 121);
+            this.Aantal_onderdelen_chart.Location = new System.Drawing.Point(86, 121);
             this.Aantal_onderdelen_chart.Name = "Aantal_onderdelen_chart";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
@@ -94,31 +119,139 @@
             this.Aantal_onderdelen_chart.Series.Add(series5);
             this.Aantal_onderdelen_chart.Series.Add(series6);
             this.Aantal_onderdelen_chart.Series.Add(series7);
-            this.Aantal_onderdelen_chart.Size = new System.Drawing.Size(294, 171);
+            this.Aantal_onderdelen_chart.Size = new System.Drawing.Size(408, 189);
             this.Aantal_onderdelen_chart.TabIndex = 1;
             this.Aantal_onderdelen_chart.Text = "chart1";
             this.Aantal_onderdelen_chart.Click += new System.EventHandler(this.chart1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(153, 89);
+            this.textBox2.Location = new System.Drawing.Point(212, 89);
             this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(136, 26);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Aantal onderdelen";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(153, 298);
+            this.textBox3.Location = new System.Drawing.Point(212, 316);
             this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(136, 26);
             this.textBox3.TabIndex = 3;
+            // 
+            // Avg_Prijzen_chart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.Avg_Prijzen_chart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.Avg_Prijzen_chart.Legends.Add(legend2);
+            this.Avg_Prijzen_chart.Location = new System.Drawing.Point(620, 121);
+            this.Avg_Prijzen_chart.Name = "Avg_Prijzen_chart";
+            this.Avg_Prijzen_chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series8.ChartArea = "ChartArea1";
+            series8.Legend = "Legend1";
+            series8.Name = "CPU";
+            series8.YValuesPerPoint = 2;
+            series9.ChartArea = "ChartArea1";
+            series9.Legend = "Legend1";
+            series9.Name = "GPU";
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "PSU";
+            series11.ChartArea = "ChartArea1";
+            series11.Legend = "Legend1";
+            series11.Name = "RAM";
+            series12.ChartArea = "ChartArea1";
+            series12.Legend = "Legend1";
+            series12.Name = "Case";
+            series13.ChartArea = "ChartArea1";
+            series13.Legend = "Legend1";
+            series13.Name = "Opslag";
+            series14.ChartArea = "ChartArea1";
+            series14.Legend = "Legend1";
+            series14.Name = "Moederbord";
+            this.Avg_Prijzen_chart.Series.Add(series8);
+            this.Avg_Prijzen_chart.Series.Add(series9);
+            this.Avg_Prijzen_chart.Series.Add(series10);
+            this.Avg_Prijzen_chart.Series.Add(series11);
+            this.Avg_Prijzen_chart.Series.Add(series12);
+            this.Avg_Prijzen_chart.Series.Add(series13);
+            this.Avg_Prijzen_chart.Series.Add(series14);
+            this.Avg_Prijzen_chart.Size = new System.Drawing.Size(369, 189);
+            this.Avg_Prijzen_chart.TabIndex = 4;
+            this.Avg_Prijzen_chart.Text = "chart1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(689, 89);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(260, 26);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Gemiddelde prijzen per component";
+            // 
+            // Totale_Prijzen_Chart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.Totale_Prijzen_Chart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.Totale_Prijzen_Chart.Legends.Add(legend3);
+            this.Totale_Prijzen_Chart.Location = new System.Drawing.Point(366, 427);
+            this.Totale_Prijzen_Chart.Name = "Totale_Prijzen_Chart";
+            this.Totale_Prijzen_Chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series15.ChartArea = "ChartArea1";
+            series15.Legend = "Legend1";
+            series15.Name = "CPU";
+            series15.YValuesPerPoint = 2;
+            series16.ChartArea = "ChartArea1";
+            series16.Legend = "Legend1";
+            series16.Name = "GPU";
+            series17.ChartArea = "ChartArea1";
+            series17.Legend = "Legend1";
+            series17.Name = "PSU";
+            series18.ChartArea = "ChartArea1";
+            series18.Legend = "Legend1";
+            series18.Name = "RAM";
+            series19.ChartArea = "ChartArea1";
+            series19.Legend = "Legend1";
+            series19.Name = "Case";
+            series20.ChartArea = "ChartArea1";
+            series20.Legend = "Legend1";
+            series20.Name = "Opslag";
+            series21.ChartArea = "ChartArea1";
+            series21.Legend = "Legend1";
+            series21.Name = "Moederbord";
+            this.Totale_Prijzen_Chart.Series.Add(series15);
+            this.Totale_Prijzen_Chart.Series.Add(series16);
+            this.Totale_Prijzen_Chart.Series.Add(series17);
+            this.Totale_Prijzen_Chart.Series.Add(series18);
+            this.Totale_Prijzen_Chart.Series.Add(series19);
+            this.Totale_Prijzen_Chart.Series.Add(series20);
+            this.Totale_Prijzen_Chart.Series.Add(series21);
+            this.Totale_Prijzen_Chart.Size = new System.Drawing.Size(405, 208);
+            this.Totale_Prijzen_Chart.TabIndex = 7;
+            this.Totale_Prijzen_Chart.Text = "chart1";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(478, 395);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(202, 26);
+            this.textBox6.TabIndex = 8;
+            this.textBox6.Text = "Totale prijs per component";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1133, 647);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.Totale_Prijzen_Chart);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Avg_Prijzen_chart);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.Aantal_onderdelen_chart);
@@ -130,6 +263,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.Aantal_onderdelen_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Avg_Prijzen_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Totale_Prijzen_Chart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +277,10 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart Aantal_onderdelen_chart;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Avg_Prijzen_chart;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Totale_Prijzen_Chart;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- Dashboard - Jason/Dashboard/Dashboard/Form1.cs
+++ Dashboard - Jason/Dashboard/Dashboard/Form1.cs
@@ -11,6 +11,7 @@ using Neo4jClient.Cypher;
 using Neo4jClient;
 using System;
 using System.IO;
+using System.Globalization;
 
 namespace Dashboard
 {
@@ -22,11 +23,27 @@ namespace Dashboard
             int aantalCPU = query_aantal("processor");
             int aantalGPU = query_aantal("videokaart");
             int aantalPSU = query_aantal("voeding");
-            int aantalRAM = query_aantal("geheugen");
+            int aantalRAM = query_aantal("werkgeheugen");
             int aantalCase = query_aantal("behuizing");
             int aantalOpslag = query_aantal("opslag");
             int aantalMoederbord = query_aantal("moederbord");
             int Totaal = aantalCPU + aantalGPU + aantalPSU + aantalRAM + aantalCase + aantalOpslag + aantalMoederbord;
+
+            decimal totaalprijsCPU = query_prijzen("processor").Item1;
+            decimal totaalprijsGPU = query_prijzen("videokaart").Item1;
+            decimal totaalprijsPSU = query_prijzen("voeding").Item1;
+            decimal totaalprijsRAM = query_prijzen("werkgeheugen").Item1;
+            decimal totaalprijsCase = query_prijzen("behuizing").Item1;
+            decimal totaalprijsOpslag = query_prijzen("opslag").Item1;
+            decimal totaalprijsMoederbord = query_prijzen("moederbord").Item1;
+
+            decimal avgprijsCPU = query_prijzen("processor").Item2;
+            decimal avgprijsGPU = query_prijzen("videokaart").Item2;
+            decimal avgprijsPSU = query_prijzen("voeding").Item2;
+            decimal avgprijsRAM = query_prijzen("werkgeheugen").Item2;
+            decimal avgprijsCase = query_prijzen("behuizing").Item2;
+            decimal avgprijsOpslag = query_prijzen("opslag").Item2;
+            decimal avgprijsMoederbord = query_prijzen("moederbord").Item2;
 
             InitializeComponent();
             Aantal_onderdelen_chart.Series["CPU"].Points.AddY(aantalCPU);
@@ -37,7 +54,29 @@ namespace Dashboard
             Aantal_onderdelen_chart.Series["Opslag"].Points.AddY(aantalOpslag);
             Aantal_onderdelen_chart.Series["Moederbord"].Points.AddY(aantalMoederbord);
             Aantal_onderdelen_chart.ChartAreas["ChartArea1"].AxisX.LabelStyle.Enabled = false;
-            textBox3.Text = Totaal.ToString();
+
+            Avg_Prijzen_chart.Series["CPU"].Points.AddY(avgprijsCPU);
+            Avg_Prijzen_chart.Series["GPU"].Points.AddY(avgprijsGPU);
+            Avg_Prijzen_chart.Series["PSU"].Points.AddY(avgprijsPSU);
+            Avg_Prijzen_chart.Series["RAM"].Points.AddY(avgprijsRAM);
+            Avg_Prijzen_chart.Series["Case"].Points.AddY(avgprijsCase);
+            Avg_Prijzen_chart.Series["Opslag"].Points.AddY(avgprijsOpslag);
+            Avg_Prijzen_chart.Series["Moederbord"].Points.AddY(avgprijsMoederbord);
+            Avg_Prijzen_chart.ChartAreas["ChartArea1"].AxisX.LabelStyle.Enabled = false;
+
+
+            Totale_Prijzen_Chart.Series["CPU"].Points.AddY(totaalprijsCPU);
+            Totale_Prijzen_Chart.Series["GPU"].Points.AddY(totaalprijsGPU);
+            Totale_Prijzen_Chart.Series["PSU"].Points.AddY(totaalprijsPSU);
+            Totale_Prijzen_Chart.Series["RAM"].Points.AddY(totaalprijsRAM);
+            Totale_Prijzen_Chart.Series["Case"].Points.AddY(totaalprijsCase);
+            Totale_Prijzen_Chart.Series["Opslag"].Points.AddY(totaalprijsOpslag);
+            Totale_Prijzen_Chart.Series["Moederbord"].Points.AddY(totaalprijsMoederbord);
+            Totale_Prijzen_Chart.ChartAreas["ChartArea1"].AxisX.LabelStyle.Enabled = false;
+
+
+            textBox3.Text = "Totaal: " + Totaal.ToString();
+            
 
         }
 
@@ -67,6 +106,31 @@ namespace Dashboard
            .ToArray();
             int Aantal = aantal_query[0].Aantal;
             return Aantal;
+        }
+
+        public Tuple<decimal, decimal> query_prijzen(string component)
+        {
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+            client.Connect();
+            var prijzen_query = client
+            .Cypher
+            .Match("(n:" + component + ")-[r:verkrijgbaar]-(m:Webshop)")
+            .Return(r => new RelationshipViewModel
+            {
+                relationshipObj = r.As<RelationshipObj>()
+            })
+           .Results
+           .ToArray();
+            decimal total_prijs = 0;
+            decimal avg_prijs = 0;
+
+            for (int i = 0; i < prijzen_query.Length; i++)
+            {
+                total_prijs = total_prijs + Convert.ToDecimal(prijzen_query[0].relationshipObj.prijs, new CultureInfo("is-IS"));
+            }
+
+            avg_prijs = total_prijs / prijzen_query.Length;
+            return Tuple.Create(total_prijs, avg_prijs);
         }
     }
     
