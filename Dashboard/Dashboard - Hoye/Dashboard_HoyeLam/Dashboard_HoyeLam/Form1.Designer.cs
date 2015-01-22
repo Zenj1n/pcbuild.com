f16da109c1356bb00629fd2212427c35b46b7502
Hoye <hoyelam96@gmail.com>
2015-01-22 18:04:39 +0100

    Dashboard maps en die van mij

---
+++ Dashboard/Dashboard - Hoye/Dashboard_HoyeLam/Dashboard_HoyeLam/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Dashboard_HoyeLam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.componenten_button = new System.Windows.Forms.Button();
+            this.prijs_webshop_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // componenten_button
+            // 
+            this.componenten_button.Location = new System.Drawing.Point(27, 44);
+            this.componenten_button.Name = "componenten_button";
+            this.componenten_button.Size = new System.Drawing.Size(171, 23);
+            this.componenten_button.TabIndex = 0;
+            this.componenten_button.Text = "componenten database";
+            this.componenten_button.UseVisualStyleBackColor = true;
+            this.componenten_button.Click += new System.EventHandler(this.componenten_button_Click);
+            // 
+            // prijs_webshop_button
+            // 
+            this.prijs_webshop_button.Location = new System.Drawing.Point(27, 232);
+            this.prijs_webshop_button.Name = "prijs_webshop_button";
+            this.prijs_webshop_button.Size = new System.Drawing.Size(171, 23);
+            this.prijs_webshop_button.TabIndex = 1;
+            this.prijs_webshop_button.Text = "Prijs verschillen webshop";
+            this.prijs_webshop_button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(383, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(383, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(386, 268);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(389, 306);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 467);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prijs_webshop_button);
+            this.Controls.Add(this.componenten_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button componenten_button;
+        private System.Windows.Forms.Button prijs_webshop_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Dashboard/Dashboard - Hoye/Dashboard_HoyeLam/Dashboard_HoyeLam/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4jClient;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Dashboard_HoyeLam
+{
+    public partial class Form1 : Form
+    {
+        public class componenten_model
+        {
+            public string naam { get; set; }
+        }
+                
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void componenten_button_Click(object sender, EventArgs e)
+        {
+           
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+            client.Connect();
+
+            //Alle componenten 
+            var totaal_componenten_query = client
+              .Cypher
+              .Match("(n)-[r:verkrijgbaar]-(p:Webshop)")
+              .Return(n => new
+              {
+                  totaal_componenten = (int)n.Count()
+              }).Results.ToArray();
+            int totaal_comp = totaal_componenten_query[0].totaal_componenten;
+
+            //Alle processoren
+            var totaal_processoren_query = client
+             .Cypher
+             .Match("(n:processor)-[r:verkrijgbaar]-(p:Webshop)")
+             .Return(n => new
+             {
+                 totaal_processor = (int)n.Count()
+             }).Results.ToArray();
+            int totaal_processoren = totaal_processoren_query[0].totaal_processor;
+
+            //Alle Moederborden
+            var totaal_moederborden_query = client
+             .Cypher
+             .Match("(n:moederbord)-[r:verkrijgbaar]-(p:Webshop)")
+             .Return(n => new
+             {
+                 totaal_moederbord = (int)n.Count()
+             }).Results.ToArray();
+            int totaal_moederborden = totaal_moederborden_query[0].totaal_moederbord;
+
+            //Alle Videokaarten
+            var totaal_videokaarten_query = client
+             .Cypher
+             .Match("(n:videokaart)-[r:verkrijgbaar]-(p:Webshop)")
+             .Return(n => new
+             {
+                 totaal_videokaart = (int)n.Count()
+             }).Results.ToArray();
+            int totaal_videokaarten = totaal_videokaarten_query[0].totaal_videokaart;
+
+            //Alle Werkgeheugen
+            var totaal_werkgeheugen_query = client
+             .Cypher
+             .Match("(n:werkgeheugen)-[r:verkrijgbaar]-(p:Webshop)")
+             .Return(n => new
+             {
+                 totaal_ram = (int)n.Count()
+             }).Results.ToArray();
+            int totaal_werkgeheugen = totaal_werkgeheugen_query[0].totaal_ram;
+
+            //Alle Behuizingen
+            var totaal_behuizingen_query = client
+             .Cypher
+             .Match("(n:behuizing)-[r:verkrijgbaar]-(p:Webshop)")
+             .Return(n => new
+             {
+                 totaal_behuizing = (int)n.Count()
+             }).Results.ToArray();
+            int totaal_behuizingen = totaal_behuizingen_query[0].totaal_behuizing;
+
+            //Alle Opslag
+            var totaal_opslag_query = client
+             .Cypher
+             .Match("(n:opslag)-[r:verkrijgbaar]-(p:Webshop)")
+             .Return(n => new
+             {
+                 totaal_opslag = (int)n.Count()
+             }).Results.ToArray();
+            int totaal_opslagen = totaal_opslag_query[0].totaal_opslag;
+
+            //Alle voeding
+            var totaal_voedingen_query = client
+             .Cypher
+             .Match("(n:voeding)-[r:verkrijgbaar]-(p:Webshop)")
+             .Return(n => new
+             {
+                 totaal_voeding = (int)n.Count()
+             }).Results.ToArray();
+            int totaal_voedingen = totaal_voedingen_query[0].totaal_voeding;
+
+            //Unused componenten in onze database
+            int niet_gebruikte_componenten = totaal_comp - totaal_behuizingen - totaal_moederborden
+                - totaal_opslagen - totaal_processoren - totaal_videokaarten - totaal_voedingen -
+                totaal_werkgeheugen;
+
+            //
+            label1.Text = "Totaal componeten in database    : " + totaal_comp.ToString();
+            label2.Text = "Totaal moederborden              : " + totaal_moederborden.ToString();
+            label3.Text = "Totaal processoren               : " + totaal_processoren.ToString();
+            label4.Text = "Totaal videokaarten              : " + totaal_videokaarten.ToString();
+            label5.Text = "Totaal werkgeheugen              : " + totaal_werkgeheugen.ToString();
+            label6.Text = "Totaal behuizingen               : " + totaal_behuizingen.ToString();
+            label7.Text = "Totaal opslag                    : " + totaal_opslagen.ToString();
+            label8.Text = "Totaal voedingen                 : " + totaal_voedingen.ToString();
+            label9.Text = "Totaal niet gebruikte componenten: " + niet_gebruikte_componenten.ToString();
+            
+        }
+
+    }
+}
