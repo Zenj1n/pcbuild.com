75366582612ef0a1dcce6e0f3456e7444e874093
Falio <f.fremouw@gmail.com>
2015-01-22 18:17:58 +0100

    test cases gefixed

--- Crawler/python unit test/Database.py
+++ Crawler/python unit test/Database.py
@@ -2,93 +2,9 @@ from py2neo import neo4j
 
 __author__ = 'Fabian'
 
-
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
-
-
-def check_dbPsu():
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`voeding`) RETURN n LIMIT 25")
-    phone = queryRead.execute()
-    for record in queryRead.stream():
-        print ""
-    return record
-
-
-def check_dbCpu():
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`processor`) RETURN n LIMIT 25")
-    phone = queryRead.execute()
-    for record in queryRead.stream():
-        print ""
-    return record
-
-
-def check_dbOpslag():
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`opslag`) RETURN n LIMIT 25")
-    phone = queryRead.execute()
-    for record in queryRead.stream():
-        print ""
-    return record
-
-
-def check_dbMB():
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`moederbord`) RETURN n LIMIT 25")
-    phone = queryRead.execute()
-    for record in queryRead.stream():
-        print ""
-    return record
-
-
-def check_dbRam():
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`werkgeheugen`) RETURN n LIMIT 25")
-    phone = queryRead.execute()
-    for record in queryRead.stream():
-        print ""
-    return record
-
-
-def check_dbCase():
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`behuizing`) RETURN n LIMIT 25")
-    phone = queryRead.execute()
-    for record in queryRead.stream():
-        print ""
-    return record
-
-
-def parse_price(response):
-    hxs = Selector(text = response)
-    titles = hxs.xpath('//ul[@id="detailview"]/li')
-    price = titles.xpath('div[@id="price"]/text()').extract()
-    return price
-
-
-def parse_desc(response):
-    hxs = Selector(text = response)
-    titles = hxs.xpath('//ul[@id="detailview"]/li')
-    desc = titles.xpath('div[@id="description"]/ul/li/text()').extract()
-    return desc
-
-
-def parse_url(response):
-    hxs = Selector(text = response)
-    titles = hxs.xpath('//ul[@id="detailview"]/li')
-    url = titles.xpath('div[@id="title"]/a/@href').extract()
-    return url
-
-
-def parse_title(response):
-    hxs = Selector(text = response)
-    titles = hxs.xpath('//ul[@id="detailview"]/li')
-    title = titles.xpath('div[@id="title"]/a/text()').extract()
-    return title
-    #image_urls = titles.xpath('div[@id="image"]/a/img/@src').extract()
 
 
 class Database(CrawlSpider):
@@ -96,13 +12,88 @@ class Database(CrawlSpider):
     allowed_domains = ["informatique.nl"]
     start_urls = ["http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800"]
 
-
-
     rules = (Rule(SgmlLinkExtractor(restrict_xpaths=('//a[contains(., "Volgende")]',))
                   , callback="parse_start_url", follow=True),
     )
 
-    #---------------------------------------------database-----------------------------------------------------------------#
+    # ---------------------------------------------database-----------------------------------------------------------------#
+
+
+
+    def parse_title(self, response):
+        hxs = Selector(text = response)
+        titles = hxs.xpath('//ul[@id="detailview"]/li')
+        title = titles.xpath('div[@id="title"]/a/text()').extract()
+        return title
+        #image_urls = titles.xpath('div[@id="image"]/a/img/@src').extract()
+
+    def parse_url(self, response):
+        hxs = Selector(text = response)
+        titles = hxs.xpath('//ul[@id="detailview"]/li')
+        url = titles.xpath('div[@id="title"]/a/@href').extract()
+        return url
+
+    def parse_desc(self, response):
+        hxs = Selector(text = response)
+        titles = hxs.xpath('//ul[@id="detailview"]/li')
+        desc = titles.xpath('div[@id="description"]/ul/li/text()').extract()
+        return desc
+
+    def parse_price(self, response):
+        hxs = Selector(text = response)
+        titles = hxs.xpath('//ul[@id="detailview"]/li')
+        price = titles.xpath('div[@id="price"]/text()').extract()
+        return price
+#---------------------------------------------database-----------------------------------------------------------------#
+    def check_dbCase(self):
+        graph_db = neo4j.GraphDatabaseService("http://Horayon:Zenjin@localhost:8080/db/data/")
+        queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`behuizing`) RETURN n LIMIT 25")
+        phone = queryRead.execute()
+        for record in queryRead.stream():
+            print ""
+        return record
+
+    def check_dbRam(self):
+        graph_db = neo4j.GraphDatabaseService("http://Horayon:Zenjin@localhost:8080/db/data/")
+        queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`werkgeheugen`) RETURN n LIMIT 25")
+        phone = queryRead.execute()
+        for record in queryRead.stream():
+            print ""
+        return record
+
+    def check_dbMB(self):
+        graph_db = neo4j.GraphDatabaseService("http://Horayon:Zenjin@localhost:8080/db/data/")
+        queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`moederbord`) RETURN n LIMIT 25")
+        phone = queryRead.execute()
+        for record in queryRead.stream():
+            print ""
+        return record
+
+
+    def check_dbOpslag(self):
+        graph_db = neo4j.GraphDatabaseService("http://Horayon:Zenjin@localhost:8080/db/data/")
+        queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`opslag`) RETURN n LIMIT 25")
+        phone = queryRead.execute()
+        for record in queryRead.stream():
+            print ""
+        return record
+
+    def check_dbCpu(self):
+        graph_db = neo4j.GraphDatabaseService("http://Horayon:Zenjin@localhost:8080/db/data/")
+        queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`processor`) RETURN n LIMIT 25")
+        phone = queryRead.execute()
+        for record in queryRead.stream():
+            print ""
+        return record
+
+    def check_dbPsu(self):
+        graph_db = neo4j.GraphDatabaseService("http://Horayon:Zenjin@localhost:8080/db/data/")
+        queryRead = neo4j.CypherQuery(graph_db,  "MATCH (n:`voeding`) RETURN n LIMIT 25")
+        phone = queryRead.execute()
+        for record in queryRead.stream():
+            print ""
+        return record
+
 
 
 
--- Crawler/python unit test/unit_test.py
+++ Crawler/python unit test/unit_test.py
@@ -13,71 +13,71 @@ class unittest(unittest.TestCase):
         cleaning = Cleaning()
         test1 = "/[CoolerMaster NSE-300-KKN1]"
         correct = "CoolerMaster NSE-300-KKN1"
-        test1 = clean_name(test1)
+        test1 = cleaning.clean_name(test1)
         self.assertEqual(test1, correct)
 
     def test_cleaningPrice(self):
         cleaning = Cleaning()
         test1 = " /[335,-]"
         correct = "335,00"
-        test1 = clean_price(test1)
+        test1 = cleaning.clean_price(test1)
         self.assertEqual(test1, correct)
 
     def test_cleanSocket(self):
         cleaning = Cleaning()
         test1 = "Socket 2011-3 (Intel)"
         correct = "2011-3"
-        test1 = clean_socket(test1)
+        test1 = cleaning.clean_socket(test1)
         self.assertEqual(test1, correct)
 
     def test_crawlNaam(self):
         database = Database()
-        databasetest = parse_title("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
+        databasetest = database.parse_title("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
         self.assertIsNot(databasetest, null)
 
     def test_crawlUrl(self):
         database = Database()
-        databasetest = parse_url("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
+        databasetest = database.parse_url("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
         self.assertIsNot(databasetest, null)
 
     def test_crawlDesc(self):
         database = Database()
-        databasetest = parse_desc("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
+        databasetest = database.parse_desc("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
         self.assertIsNot(databasetest, null)
 
     def test_crawlPrice(self):
         database = Database()
-        databasetest = parse_price("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
+        databasetest = database.parse_price("http://www.informatique.nl/?m=usl&g=004&view=6&&sort=pop&pl=800")
         self.assertIsNot(databasetest, null)
 
     def test_dbCase(self):
         database = Database()
-        databasetest = check_dbCase()
+        databasetest = database.check_dbCase()
         self.assertIsNot(databasetest, null)
 
     def test_dbRam(self):
         database = Database()
-        databasetest = check_dbRam()
+        databasetest = database.check_dbRam()
         self.assertIsNot(databasetest, null)
 
     def test_dbMB(self):
         database = Database()
-        databasetest = check_dbMB()
+        databasetest = database.check_dbMB()
         self.assertIsNot(databasetest, null)
 
     def test_dbOpslag(self):
         database = Database()
-        databasetest = check_dbOpslag()
+        databasetest = database.check_dbOpslag()
         self.assertIsNot(databasetest, null)
 
     def test_dbCpu(self):
         database = Database()
-        databasetest = check_dbCpu()
+        databasetest = database.check_dbCpu()
         self.assertIsNot(databasetest, null)
 
     def test_dbPsu(self):
         database = Database()
-        databasetest = check_dbPsu()
+        databasetest = database.check_dbPsu()
         self.assertIsNot(databasetest, null)
 
 
