533aecad962e35511ea24054fafb5d752cc2c1c0
Jtlie <jtlie@live.nl>
2015-01-23 07:09:26 +0100

    Merge branch 'master' of https://github.com/Zenj1n/pcbuild.com
    Conflicts:
    	Crawler/r script c/r script c.v12.suo

--- Crawler/r script c/r script c/Program.cs
+++ Crawler/r script c/r script c/Program.cs
@@ -9,17 +9,24 @@ using System.Text.RegularExpressions;
 using System.Diagnostics;
 namespace r_script_c
 {
+    /*Script doet het wel op m'n desktop maar niet op de server. 
+     * Op m'n desktop wacht hij nu tot dat de vorige cmd.exe 
+     * is afgesloten voordat hij de volgende opent. Op de server 
+     * loopt hij nogsteeds van en duurt ongeveer 20 min voordat 
+     * je weer wat kan doen 
+     */
+     
     class Program
     {
         static void Main(string[] args)
         {
             CPU_grafiek();
-            //GPU_grafiek();
-            //PSU_grafiek();
-            //Moederbord_grafiek();
-            //Case_grafiek();
-            //RAM_grafiek();
-            //Opslag_grafiek();                 
+            GPU_grafiek();
+            PSU_grafiek();
+            Moederbord_grafiek();
+            Case_grafiek();
+            RAM_grafiek();
+            Opslag_grafiek();
         }
         public static void Run_Rscript(string component)
         {
@@ -28,27 +35,30 @@ namespace r_script_c
             ProcessStartInfo start = new ProcessStartInfo("cmd.exe");
             start.Arguments = strCmdText;
             Process cmd = Process.Start(start);
+            Console.WriteLine(strCmdText);
+            Console.WriteLine("Prijshistory van " + component + " aan get maken");
             cmd.WaitForExit();
+            Console.ReadKey();
         }
 
-        //public static void Moederbord_grafiek()
-        //{
-        //    string component;
-        //    var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
-        //    client.Connect();
-        //    var query_componenten = client
-        //   .Cypher
-        //   .Match("(n:moederbord)")
-        //   .Return(n => n.As<moederbord>())
-        //   .Results
-        //   .ToArray();
-        //    for (int i = 0; i < query_componenten.Length; i++)
-        //    {
-        //        component = query_componenten[i].naam;
-        //        component = Regex.Replace(component," ","_");
-        //        Run_Rscript(component);
-        //    }
-        //}
+        public static void Moederbord_grafiek()
+        {
+            string component;
+            var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
+            client.Connect();
+            var query_componenten = client
+           .Cypher
+           .Match("(n:moederbord)")
+           .Return(n => n.As<moederbord>())
+           .Results
+           .ToArray();
+            for (int i = 0; i < query_componenten.Length; i++)
+            {
+                component = query_componenten[i].naam;
+                component = Regex.Replace(component, " ", "_");
+                Run_Rscript(component);
+            }
+        }
 
         public static void CPU_grafiek()
         {
@@ -61,114 +71,114 @@ namespace r_script_c
            .Return(n => n.As<processor>())
            .Results
            .ToArray();
-            //for (int i = 0; i < 1; i++)
-            //{
-            //    try
-            //    {
-                    component = query_componenten[1].naam;
+            for (int i = 0; i < query_componenten.Length; i++)
+            {
+                try
+                {
+                    component = query_componenten[i].naam;
                     component = Regex.Replace(component, " ", "_");
                     Run_Rscript(component);
-            //    }
-            //    catch
-            //    {
-            //        break;
-            //    }
+                }
+                catch
+                {
+                    break;
+                }
 
-            //}
+            }
         }
 
-        //public static void GPU_grafiek()
-        //{
-        //    string component;
-        //    var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
-        //    client.Connect();
-        //    var query_componenten = client
-        //   .Cypher
-        //   .Match("(n:videokaart)")
-        //   .Return(n => n.As<videokaart>())
-        //   .Results
-        //   .ToArray();
-        //    for (int i = 0; i < query_componenten.Length; i++)
-        //    {
-        //        component = query_componenten[i].naam;
-        //        component = Regex.Replace(component, " ", "_");
-        //        Run_Rscript(component);
-        //    }
-        //}
+        public static void GPU_grafiek()
+        {
+            string component;
+            var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
+            client.Connect();
+            var query_componenten = client
+           .Cypher
+           .Match("(n:videokaart)")
+           .Return(n => n.As<videokaart>())
+           .Results
+           .ToArray();
+            for (int i = 0; i < query_componenten.Length; i++)
+            {
+                component = query_componenten[i].naam;
+                component = Regex.Replace(component, " ", "_");
+                Run_Rscript(component);
+            }
+        }
 
-        //public static void RAM_grafiek()
-        //{
-        //    string component;
-        //    var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
-        //    client.Connect();
-        //    var query_componenten = client
-        //   .Cypher
-        //   .Match("(n:werkgeheugen)")
-        //   .Return(n => n.As<werkgeheugen>())
-        //   .Results
-        //   .ToArray();
-        //    for (int i = 0; i < query_componenten.Length; i++)
-        //    {
-        //        component = query_componenten[i].naam;
-        //        component = Regex.Replace(component, " ", "_");
-        //        Run_Rscript(component);
-        //    }
-        //}
-        //public static void Case_grafiek()
-        //{
-        //    string component;
-        //    var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
-        //    client.Connect();
-        //    var query_componenten = client
-        //   .Cypher
-        //   .Match("(n:behuizing)")
-        //   .Return(n => n.As<behuizing>())
-        //   .Results
-        //   .ToArray();
-        //    for (int i = 0; i < query_componenten.Length; i++)
-        //    {
-        //        component = query_componenten[i].naam;
-        //        component = Regex.Replace(component, " ", "_");
-        //        Run_Rscript(component);
-        //    }
-        //}
-        //public static void Opslag_grafiek()
-        //{
-        //    string component;
-        //    var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
-        //    client.Connect();
-        //    var query_componenten = client
-        //   .Cypher
-        //   .Match("(n:opslag)")
-        //   .Return(n => n.As<opslag>())
-        //   .Results
-        //   .ToArray();
-        //    for (int i = 0; i < query_componenten.Length; i++)
-        //    {
-        //        component = query_componenten[i].naam;
-        //        component = Regex.Replace(component, " ", "_");
-        //        Run_Rscript(component);
-        //    }
-        //}
+        public static void RAM_grafiek()
+        {
+            string component;
+            var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
+            client.Connect();
+            var query_componenten = client
+           .Cypher
+           .Match("(n:werkgeheugen)")
+           .Return(n => n.As<werkgeheugen>())
+           .Results
+           .ToArray();
+            for (int i = 0; i < query_componenten.Length; i++)
+            {
+                component = query_componenten[i].naam;
+                component = Regex.Replace(component, " ", "_");
+                Run_Rscript(component);
+            }
+        }
+        public static void Case_grafiek()
+        {
+            string component;
+            var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
+            client.Connect();
+            var query_componenten = client
+           .Cypher
+           .Match("(n:behuizing)")
+           .Return(n => n.As<behuizing>())
+           .Results
+           .ToArray();
+            for (int i = 0; i < query_componenten.Length; i++)
+            {
+                component = query_componenten[i].naam;
+                component = Regex.Replace(component, " ", "_");
+                Run_Rscript(component);
+            }
+        }
+        public static void Opslag_grafiek()
+        {
+            string component;
+            var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
+            client.Connect();
+            var query_componenten = client
+           .Cypher
+           .Match("(n:opslag)")
+           .Return(n => n.As<opslag>())
+           .Results
+           .ToArray();
+            for (int i = 0; i < query_componenten.Length; i++)
+            {
+                component = query_componenten[i].naam;
+                component = Regex.Replace(component, " ", "_");
+                Run_Rscript(component);
+            }
+        }
 
-        //public static void PSU_grafiek()
-        //{
-        //    string component;
-        //    var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
-        //    client.Connect();
-        //    var query_componenten = client
-        //   .Cypher
-        //   .Match("(n:voeding)")
-        //   .Return(n => n.As<voeding>())
-        //   .Results
-        //   .ToArray();
-        //    for (int i = 0; i < query_componenten.Length; i++)
-        //    {
-        //        component = query_componenten[i].naam;
-        //        component = Regex.Replace(component, " ", "_");
-        //        Run_Rscript(component);
-        //    }
-        //}
+        public static void PSU_grafiek()
+        {
+            string component;
+            var client = new GraphClient(new Uri("http://Horayon:Zenjin@localhost:8080/db/data"));
+            client.Connect();
+            var query_componenten = client
+           .Cypher
+           .Match("(n:voeding)")
+           .Return(n => n.As<voeding>())
+           .Results
+           .ToArray();
+            for (int i = 0; i < query_componenten.Length; i++)
+            {
+                component = query_componenten[i].naam;
+                component = Regex.Replace(component, " ", "_");
+                Run_Rscript(component);
+            }
+        }
 
     }
 }
